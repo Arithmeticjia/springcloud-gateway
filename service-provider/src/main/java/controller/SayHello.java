743f92de47644736f84db30b7100c896d075171a
ArithmeticJia <1097197237@qq.com>
2020-11-30 17:04:49 +0800

    Initial commit

---
+++ service-provider/src/main/java/controller/SayHello.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class SayHello {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "hello";
+    }
+}
---
+++ service-register/src/main/java/com/springcloud/register/RegisterApplication.java
@@ -0,0 +1,15 @@
+package com.springcloud.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+
+}
